1ca9220813bb8edeb49c54dc2666be064f3bcc2a
n333k0 <147572862+n333k0@users.noreply.github.com>
2024-06-29 01:55:15 -0300

    Logo Change 2

--- packages/ui/src/ui-component/extended/Logo.jsx
+++ packages/ui/src/ui-component/extended/Logo.jsx
@@ -1,5 +1,5 @@
-import logo from '@/assets/images/flowise_logo.png'
-import logoDark from '@/assets/images/flowise_logo_dark.png'
+import logo from '@/assets/images/logo.png'
+import logoDark from '@/assets/images/logo_dark.png'
 
 import { useSelector } from 'react-redux'
 
